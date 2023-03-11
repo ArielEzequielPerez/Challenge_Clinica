793411795300e0063249cf61b0c9febd3f77a1f8
ArielEzequilPerez <ariperez@frba.utn.edu.ar>
2023-03-11 12:40:05 -0300

    se agrego algunos cambios en los estilos en pacientes

--- src/components/Pacientes.vue
+++ src/components/Pacientes.vue
@@ -1,27 +1,43 @@
-
 <template>
-   <table class="table stripped bordered">
-      <thead>
-         <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Consulta</th>
-            <th></th>
-         </tr>
+  <div class="container">
+    <table class="table">
+      <thead class="table-dark">
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Consulta</th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
-         <tr v-for="paciente in pacientes">
-            <td>{{ paciente.historial }}</td>
-            <td>{{ paciente.nombre }}</td>
-            <td> <a> <i class="fa-sharp fa-solid fa-list-ul"></i> </a> Consultas </td>
-         </tr>
+        <tr v-for="paciente in pacientes">
+          <td>{{ paciente.historial }}</td>
+          <td>{{ paciente.nombre }}</td>
+          <td>
+            <tr class="table" v-for="consulta in paciente.consutas">
+              <td>
+               <div>
+                  <p>Dolencia: <em>{{ consulta.dolencia }}</em> </p> 
+                  <p>Suceso: <em>{{ consulta.suceso }}</em> </p>
+               </div>
+               </td>
+              
+            </tr>
+          </td>
+        </tr>
       </tbody>
-   </table>
+    </table>
+  </div>
 </template>
 <script setup lang="ts">
-import pacientes from '../../datosDeTablas/paciente.json'
-import { onMounted, ref , reactive } from 'vue';
+import pacientes from "../../datosDeTablas/paciente.json";
+import { onMounted, ref, reactive } from "vue";
 
 // Aca deberia estar conectado con una api
-
 </script>
+<style>
+.tr {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,4 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).use(router).mount('#app')
