bcc0d385f948bb9d214a5719011fceecb714551c
ArielEzequilPerez <ariperez@frba.utn.edu.ar>
2023-03-11 11:55:02 -0300

    update de paciente

--- src/components/Pacientes.vue
+++ src/components/Pacientes.vue
@@ -11,7 +11,7 @@
       </thead>
       <tbody>
          <tr v-for="paciente in pacientes">
-            <td>{{ paciente.historia_clinica }}</td>
+            <td>{{ paciente.historial }}</td>
             <td>{{ paciente.nombre }}</td>
             <td> <a> <i class="fa-sharp fa-solid fa-list-ul"></i> </a> Consultas </td>
          </tr>
@@ -19,12 +19,9 @@
    </table>
 </template>
 <script setup lang="ts">
-import pacienteConsultas from '../../datosDeTablas/paciente.json'
+import pacientes from '../../datosDeTablas/paciente.json'
 import { onMounted, ref , reactive } from 'vue';
-var pacientes = ref(null)
 
-onMounted(fetchPacientes)
-function fetchPacientes(){
-  pacientes.value = fetch(../../datosDeTablas/paciente.json).then(res => res.json()
-}
+// Aca deberia estar conectado con una api
+
 </script>
